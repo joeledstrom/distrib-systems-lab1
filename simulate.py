a9f0174329dc997ae341700a7e3112b4cac40ec1
Joel Edström <joel.edstrom@gmail.com>
2017-09-20 12:47:38 +0200

    Add simulate.py comments

--- simulate.py
+++ simulate.py
@@ -8,17 +8,20 @@ t.addChannel("SimpleTransceiverC", sys.stdout)
 #t.addChannel("TestACK", sys.stdout)
 
 
-
+# Create 10 nodes
 nodes = [t.getNode(x) for x in range(10)]
 
+# Connect a pair of nodes
 def connect(x,y):
     if y:
         r.add(x.id(), y.id(), -60.0)
         r.add(y.id(), x.id(), -60.0)
     return y
 
+# Abuse reduce function to create a circle of connected nodes
 reduce(connect, nodes + nodes[:1])
 
+# Add noise trace readings to all nodes
 noise = open("radio-noise.txt", "r")
 lines = noise.readlines()
 for line in lines:
